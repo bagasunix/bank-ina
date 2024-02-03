0b91274a1df399553399b1c74fa3c8798fa396f0
Aldino Pratama <aldinopratama15@gmail.com>
2024-02-03 21:06:49 +0700

    create service init

---
+++ server/cmd/inits/service.go
@@ -0,0 +1,26 @@
+package inits
+
+import (
+	"go.uber.org/zap"
+
+	"github.com/bagasunix/bank-ina/server/domains"
+	"github.com/bagasunix/bank-ina/server/domains/data/repositories"
+	"github.com/bagasunix/bank-ina/server/domains/middlewares"
+)
+
+func InitService(logger *zap.Logger, repo repositories.Repositories) domains.Service {
+	serviceBuilder := domains.NewServiceBuilder(logger, repo)
+	serviceBuilder.SetMiddlewares(getServiceMiddleware(logger))
+	return serviceBuilder.Build()
+}
+
+func getServiceMiddleware(logger *zap.Logger) []domains.Middleware {
+	var mw []domains.Middleware
+	var repo repositories.Repositories
+	mw = addDefaultServiceMiddleware(logger, mw, repo)
+	return mw
+}
+
+func addDefaultServiceMiddleware(logger *zap.Logger, mw []domains.Middleware, repo repositories.Repositories) []domains.Middleware {
+	return append(mw, middlewares.LoggingMiddleware(logger, repo))
+}
