934101618c5906c78298fffedb442a64f5bf23cd
Aldino Pratama <aldinopratama15@gmail.com>
2024-02-04 00:09:12 +0700

    update task endpoint

--- server/endpoints/task.go
+++ server/endpoints/task.go
@@ -18,24 +18,24 @@ const (
 type TaskEndpoint struct {
 	CreateTaskEndpoint Endpoint
 	ListTaskEndpoint   Endpoint
-	UpdateMovieEnpoint Endpoint
+	UpdateTaskEnpoint  Endpoint
 	ViewTaskEndpoint   Endpoint
-	DeleteMovieEnpoint Endpoint
+	DeleteTaskEnpoint  Endpoint
 }
 
 func NewTaskEndpoint(s domains.Service, mdw map[string][]Middleware) TaskEndpoint {
 	eps := TaskEndpoint{}
 	eps.CreateTaskEndpoint = makeCreateTaskEndpoint(s)
 	eps.ListTaskEndpoint = makeListTaskEndpoint(s)
-	eps.UpdateMovieEnpoint = makeUpdateTaskEndpoint(s)
+	eps.UpdateTaskEnpoint = makeUpdateTaskEndpoint(s)
 	eps.ViewTaskEndpoint = makeViewTaskEndpoint(s)
-	eps.DeleteMovieEnpoint = makeDeleteTaskEndpoint(s)
+	eps.DeleteTaskEnpoint = makeDeleteTaskEndpoint(s)
 
 	SetEndpoint(CREATE_TASK, &eps.CreateTaskEndpoint, mdw)
 	SetEndpoint(LIST_TASK, &eps.ListTaskEndpoint, mdw)
-	SetEndpoint(UPDATE_TASK, &eps.UpdateMovieEnpoint, mdw)
+	SetEndpoint(UPDATE_TASK, &eps.UpdateTaskEnpoint, mdw)
 	SetEndpoint(View_TASK, &eps.ViewTaskEndpoint, mdw)
-	SetEndpoint(DELETE_TASK, &eps.DeleteMovieEnpoint, mdw)
+	SetEndpoint(DELETE_TASK, &eps.DeleteTaskEnpoint, mdw)
 
 	return eps
 }
