1705ff0636b7c6d3b26526e1979ccac85da5007c
Aldino Pratama <aldinopratama15@gmail.com>
2024-02-03 23:37:39 +0700

    create auth middleware

---
+++ server/endpoints/middlewares/auth.go
@@ -0,0 +1,48 @@
+package middlewares
+
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+
+	"github.com/bagasunix/bank-ina/pkg/errors"
+	"github.com/bagasunix/bank-ina/pkg/jwt"
+	"github.com/bagasunix/bank-ina/server/endpoints"
+)
+
+const (
+	authorizationHeaderKey             = "authorization"
+	authorizationTypeBearer            = "bearer"
+	authorizationPayloadKey            = "authorization_payload"
+	authorizationIntegrationPayloadKey = "authorization_payload_integration"
+)
+
+func Authentication(logs *zap.Logger) endpoints.Middleware {
+	return func(e endpoints.Endpoint) endpoints.Endpoint {
+		return func(ctx *gin.Context, request any) (response any, err error) {
+			tokenString := ctx.GetHeader(authorizationHeaderKey)
+			if tokenString == "" {
+				return nil, errors.ErrUnAuthorized()
+			}
+
+			fields := strings.Fields(tokenString)
+			if len(fields) < 2 {
+				return nil, errors.ErrUnAuthorized()
+			}
+
+			authorizationType := strings.ToLower(fields[0])
+			if authorizationType != authorizationTypeBearer {
+				return nil, errors.ErrUnAuthorized()
+			}
+
+			claims, err := jwt.ValidateToken(fields[1])
+			if err != nil {
+				return nil, err
+			}
+
+			ctx.Set(authorizationPayloadKey, claims.User)
+			return e(ctx, request)
+		}
+	}
+}
