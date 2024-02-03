6e28e4f2e7a4b931505506005c5daa1f5c75e3fc
Aldino Pratama <aldinopratama15@gmail.com>
2024-02-03 20:59:08 +0700

    create task endpoint

---
+++ server/endpoints/task.go
@@ -0,0 +1,70 @@
+package endpoints
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/bagasunix/bank-ina/server/domains"
+	"github.com/bagasunix/bank-ina/server/endpoints/requests"
+)
+
+const (
+	CREATE_TASK = "CreateTask"
+	UPDATE_TASK = "UpdateTask"
+	DELETE_TASK = "DeleteTask"
+	View_TASK   = "ViewTask"
+	LIST_TASK   = "ListTask"
+)
+
+type TaskEndpoint struct {
+	CreateTaskEndpoint Endpoint
+	ListTaskEndpoint   Endpoint
+	UpdateMovieEnpoint Endpoint
+	ViewTaskEndpoint   Endpoint
+	DeleteMovieEnpoint Endpoint
+}
+
+func NewTaskEndpoint(s domains.Service, mdw map[string][]Middleware) TaskEndpoint {
+	eps := TaskEndpoint{}
+	eps.CreateTaskEndpoint = makeCreateTaskEndpoint(s)
+	eps.ListTaskEndpoint = makeListTaskEndpoint(s)
+	eps.UpdateMovieEnpoint = makeUpdateTaskEndpoint(s)
+	eps.ViewTaskEndpoint = makeViewTaskEndpoint(s)
+	eps.DeleteMovieEnpoint = makeDeleteTaskEndpoint(s)
+
+	SetEndpoint(CREATE_TASK, &eps.CreateTaskEndpoint, mdw)
+	SetEndpoint(LIST_TASK, &eps.ListTaskEndpoint, mdw)
+	SetEndpoint(UPDATE_TASK, &eps.UpdateMovieEnpoint, mdw)
+	SetEndpoint(View_TASK, &eps.ViewTaskEndpoint, mdw)
+	SetEndpoint(DELETE_TASK, &eps.DeleteMovieEnpoint, mdw)
+
+	return eps
+}
+
+func makeCreateTaskEndpoint(s domains.Service) Endpoint {
+	return func(ctx *gin.Context, request any) (response any, err error) {
+		return s.CreateTask(ctx, request.(*requests.CreateTask))
+	}
+}
+
+func makeUpdateTaskEndpoint(s domains.Service) Endpoint {
+	return func(ctx *gin.Context, request any) (response any, err error) {
+		return s.UpdateTask(ctx, request.(*requests.UpdateTask))
+	}
+}
+
+func makeDeleteTaskEndpoint(s domains.Service) Endpoint {
+	return func(ctx *gin.Context, request any) (response any, err error) {
+		return s.DeleteTask(ctx, request.(*requests.EntityId))
+	}
+}
+
+func makeListTaskEndpoint(s domains.Service) Endpoint {
+	return func(ctx *gin.Context, request any) (response any, err error) {
+		return s.GetAllTask(ctx)
+	}
+}
+func makeViewTaskEndpoint(s domains.Service) Endpoint {
+	return func(ctx *gin.Context, request any) (response any, err error) {
+		return s.ViewTask(ctx, request.(*requests.EntityId))
+	}
+}
