00e9e684de8e87975a528c338db31ffc8d649e7a
Aldino Pratama <aldinopratama15@gmail.com>
2024-02-04 02:00:02 +0700

    update service usecase

--- server/domains/service.go
+++ server/domains/service.go
@@ -1,6 +1,7 @@
 package domains
 
 import (
+	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 
 	"github.com/bagasunix/bank-ina/server/domains/data/repositories"
@@ -10,30 +11,36 @@ import (
 type Service interface {
 	usecases.User
 	usecases.Task
+	usecases.Auth
 }
 
 type service struct {
 	usecases.User
 	usecases.Task
+	usecases.Auth
 }
 
 type ServiceBuilder struct {
 	logger       *zap.Logger
 	repositories repositories.Repositories
+	jwtKey       string
+	ctx          *gin.Context
 	middlewares  []Middleware
 }
 
-func NewServiceBuilder(logger *zap.Logger, repositories repositories.Repositories) *ServiceBuilder {
+func NewServiceBuilder(logger *zap.Logger, jwtKey string, repositories repositories.Repositories) *ServiceBuilder {
 	a := new(ServiceBuilder)
 	a.logger = logger
 	a.repositories = repositories
+	a.jwtKey = jwtKey
 	return a
 }
 
-func buildService(logger *zap.Logger, repo repositories.Repositories) Service {
+func buildService(logger *zap.Logger, repo repositories.Repositories, jwtKey string) Service {
 	svc := new(service)
 	svc.User = usecases.NewUser(logger, repo)
 	svc.Task = usecases.NewTask(logger, repo)
+	svc.Auth = usecases.NewAuthentication(logger, jwtKey, repo)
 	return svc
 }
 
@@ -44,7 +51,7 @@ func (s *ServiceBuilder) SetMiddlewares(middlewares []Middleware) *ServiceBuilde
 }
 
 func (s *ServiceBuilder) Build() Service {
-	svc := buildService(s.logger, s.repositories)
+	svc := buildService(s.logger, s.repositories, s.jwtKey)
 	for _, m := range s.middlewares {
 		svc = m(s.repositories, svc)
 	}
